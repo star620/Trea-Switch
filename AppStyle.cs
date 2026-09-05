50b72b6c1d2e39b67e65804267f8252e57054e89
star620 <1011819146@qq.com>
2026-09-05 23:32:33 +0800

    chore: scaffold TraeSwitch WinForms app with test project

---
+++ AppStyle.cs
@@ -0,0 +1,9 @@
+namespace TraeSwitch;
+
+public static class AppStyle
+{
+    public static readonly Color SideBack = Color.FromArgb(0x1E, 0x29, 0x3B);
+    public static readonly Color Accent = Color.FromArgb(0x3B, 0x82, 0xF6);
+    public static readonly Color ContentBack = Color.FromArgb(0xF1, 0xF5, 0xF9);
+    public static readonly Color TextDark = Color.FromArgb(0x0F, 0x17, 0x2A);
+}
---
+++ MainForm.cs
@@ -0,0 +1,12 @@
+namespace TraeSwitch;
+
+public class MainForm : Form
+{
+    public MainForm()
+    {
+        Text = "TraeSwitch 账号冷切换助手";
+        ClientSize = new Size(880, 560);
+        BackColor = AppStyle.ContentBack;
+        StartPosition = FormStartPosition.CenterScreen;
+    }
+}
